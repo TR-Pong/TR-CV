8687b41257d263f17ff55df3fdaf89a5cbcabad2
TR PONG <tanakhom.rattana@hotmail.com>
2026-01-26 17:08:06 +0700

    update exp component

--- src/components/ExpView.vue
+++ src/components/ExpView.vue
@@ -14,15 +14,25 @@
             name: "Outsourcify Co.,LTD",
             timestart: "2018-03-01",
             timeend: "2020-05-01",
-            desc: "Develop website e-learning about luxury brand",
-            icon: "city"
+            icon: "city",
+            desc: [
+                "Developed high-quality e-learning websites for sales staff of a French luxury brand, aimed at enhancing product knowledge and sales effectiveness.",
+                "Evaluated and planned website development strategically by analyzing client requirements and aligning with appropriate marketing objectives.",
+                "Coordinated with clients from France to ensure all project deliverables met expectations and were executed accurately without errors."
+            ]
+            
         },
         {
             name: "Tigon Tech Co.,LTD",
             timestart: "2020-05-11",
             timeend: "2025-12-31",
-            desc: "Develop web and game",
-            icon: "briefcase"
+            icon: "briefcase",
+            desc: [
+                "Developed and designed websites using Vue.js, focusing on performance, scalability, and user-centered experiences.",
+                "Created games using Unity and Cocos Creator, with an emphasis on developing new features and improving overall gameplay experience.",
+                "Researched and applied new technologies and techniques to enhance team productivity and deliver higher-quality creative outcomes.",
+                "Mentored and supported junior team members, helping them build technical skills, confidence, and effectiveness in their roles."
+            ]
         }
     ]
 </script>
--- src/components/subcomponent/Exp.vue
+++ src/components/subcomponent/Exp.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="Exp w-full" data-aos="zoom-in">
         <div class="my-1 w-full flex flex-wrap bg-white rounded-2xl min-w-90 py-5">
-            <div class="px-6 py-0 md:py-5 text-left year min-w-50 w-full lg:w-auto flex flex-col justify-center items-center lg:items-start">
+            <div class="px-6 py-0 md:py-2 text-left year min-w-50 w-full lg:w-auto flex flex-col justify-center items-center lg:items-start md:bg-Secoundary md:rounded-4xl md:ml-10 md:shadow-sm md:border-[0.01rem] md:border-TextPrimary">
                 <div class="text-Primary text-2xl">{{ new Date(DateStart).getFullYear() }} - {{ DateEnd == ""? "Present" : new Date(DateEnd).getFullYear() }}</div>
                 <div class="">{{ diff.years }} year {{ diff.months }} month</div>
             </div>
-             <div class="px-6 py-5 w-full md:w-auto flex flex-col md:flex-row justify-center md:justify-start items-center h-auto flex-wrap min-w-90">
-                <fa :icon="['fas',FaIcon]" class="fa-2xl text-Primary px-4 py-5" />
-                <div class="flex flex-col text-center  md:text-left">
+             <div class="px-6 py-3 w-full md:w-auto flex flex-col justify-center md:justify-start items-center md:items-start h-auto flex-wrap min-w-90">
+                 <div class="flex flex-row justify-center md:justify-start items-center text-center md:text-left">
+                    <fa :icon="['fas',FaIcon]" class="fa-2xl text-Primary px-4 py-5" />
                      <div class="shink-0 text-Primary Company text-2xl">
                         {{ CompName }}
                     </div>
-                    <div class="shink-0 Company_desc">{{ JobDesc }}</div>
                 </div> 
+                <div class="desclist text-left pl-5 md:pl-10">
+                    <li v-for="desc in JobDesc" class="shink-0 py-1 Company_desc break-before-all">{{ desc }}</li>
+                </div>
             </div>
         </div>
     </div>
@@ -42,6 +44,6 @@
     }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
